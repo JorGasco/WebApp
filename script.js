8d31bcf6bfdc8312685c88fa59f00d46cccacab6
Glitch (glitch-blank-website) <none>
2023-01-19 13:42:35 +0000

    👁🌝 Checkpoint ./index.html:53479949/421 ./.glitch-assets:53479949/363 ./script.js:53479949/1187

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-console.log("Hello, world!");
+const multi = `Once upon a time,
+In a land far far away,
+there lived a witch,
+who could change night into day`
+console.log(multi)
+
+
+let a = prompt("first number");
+let b = prompt("second number");
+
+function multiply(a, b) {
+  return a * b
+}
+
+let answer = multiply(a, b)
+console.log(answer)
+
+document.write("you answer is " + answer + " ! <br>");
