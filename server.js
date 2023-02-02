f9db12fad95d5b8ab4182d9ade65b4ba6b29011a
Glitch (glitch-blank-website) <none>
2023-02-02 13:32:44 +0000

    ⛷😻 Checkpoint ./package.json:53479949/251 ./server.js:53479949/2111 ./views/layouts/main.hbs:53479949/1192 ./views/partials/menu.hbs:53479949/301

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // use javascript in strict mode
-'use strict';
+"use strict";
 
 // import all required modules
 import express from "express";
-import path from 'path';
+import exphbs from "express-handlebars";
 import logger from "./utils/logger.js";
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 // initialise project
@@ -15,6 +16,11 @@ const __dirname = path.dirname(__filename);
 // static files output to public folder
 app.use(express.static("public"));
 
+// use handlebars as view engine
+const handlebars = exphbs.create({ extname: ".hbs" });
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -22,5 +28,5 @@ app.get("/", function(request, response) {
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 4000, function () {
-  logger.info('Your app is listening on port ' + listener.address().port);
+  logger.info("Your app is listening on port " + listener.address().port);
 });
