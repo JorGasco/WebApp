58d3d5c61865478ed713796930af4e9fa297d7dd
Glitch (glitch-blank-website) <none>
2023-04-25 23:23:20 +0000

    🚏👨 Checkpoint ./controllers/welcome.js:53479949/394 ./views/welcome.hbs:53479949/727

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -2,6 +2,7 @@
 
 // import all required modules
 import logger from '../utils/logger.js';
+import basketStore from '../models/basket-store.js';
 
 // create welcome object
 const welcome = {
@@ -9,12 +10,29 @@ const welcome = {
   // index method - responsible for creating and rendering the view
   index(request, response) {
 
+    //app Calculations
+const baskets = basketStore.getAllBaskets();
+    let numBaskets = baskets.lenght;
+    let numProducts = 0;
+
+
+    for (let item of baskets){
+      numProducts += item.products.lenght;
+    }
+    
+    
+    
+    
+    
     // display confirmation message in log
     logger.info('welcome rendering');
 
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
       title: 'Welcome Company Product Collection',
+      totalBaskets: numBaskets,
+      totalSongs: numProducts,
+      
     };
 
     // render the start view and pass through the data
