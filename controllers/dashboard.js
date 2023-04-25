2fe32d644df0e2da87e5dfca8599df582cdbe25e
Glitch (glitch-blank-website) <none>
2023-04-25 02:44:57 +0000

    🏥💂 Checkpoint ./controllers/dashboard.js:53479949/4890 ./models/json-store.js:53479949/3370

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,25 +2,49 @@
 
 // import all required modules
 import logger from '../utils/logger.js';
-import productStore from '../models/product-store.js';
+import { v4 as uuidv4 } from 'uuid';
+import basketStore from '../models/basket-store.js';
 
 // create dashboard object
-const dashboard = {
+const  dashboard = {
 
 // index method - responsible for creating and rendering the view
-index(request, response) {// display confirmation message in log
+index(request, response) {
+// display confirmation message in log
 logger.info('dashboard rendering');
 
 // create view data object (contains data to be sent to the view e.g. page title)
 const viewData = {
-  title: 'Company Product Collection Dashboard',
-  products: productStore.getAllProducts(),
+  title: 'Basket App Dashboard',
+  baskets: basketStore.getAllBaskets(),
 };
 
 // render the dashboard view and pass through the data
-logger.info('about to render', viewData.products);
+logger.info('about to render', viewData.baskets);
 response.render('dashboard', viewData);
   },
+
+deleteBasket(request, response) {
+const basketId = request.params.id;
+logger.debug('Deleting Basket ${basketId}');
+basketStore.removeBasket(basketId);
+response.redirect('/dasboard');
+},
+
+
+
+
+
+
+addBasket(request, response) {
+const newBasket = {
+id: uuidv4(),
+name: request.body.name,
+items: [],
+};
+basketStore.addBasket(newBasket);
+response.redirect('/dashboard');
+},
 };
 
 // export the dashboard module
