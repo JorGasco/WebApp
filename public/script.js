04030f96637d12713e3452e856232fec30467c9d
Glitch (glitch-blank-website) <none>
2023-04-25 02:56:55 +0000

    🚀🚀 Checkpoint ./public/script.js:53479949/134 ./utils/logger.js:53479949/519 ./views/layouts/main.hbs:53479949/2446 ./views/partials/addProduct.hbs:53479949/980

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
 import basketStore from './models/basket-store.js';
 import productStore from './models/product-store.js';
 // Import Handlebars
+$(".delsong").click(() => confirm('Really delete this song?'))
+
+$(".delplaylist").click(() => confirm('Really delete this playlist?'))
