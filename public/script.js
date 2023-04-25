d1db90556d2065ad22e17ad40627e8b8261c259c
Glitch (glitch-blank-website) <none>
2023-04-25 01:56:55 +0000

    🎡🐩 Checkpoint ./public/script.js:53479949/830 ./views/basket.hbs:53479949/1602 ./views/partials/prod.hbs:53479949/137

--- public/script.js
+++ public/script.js
@@ -1,33 +1,3 @@
 import basketStore from './models/basket-store.js';
 import productStore from './models/product-store.js';
 // Import Handlebars
-import Handlebars from 'handlebars';
-
-function renderBasket() {
-  const basketTemplateSource = $('#basket-template').html();
-  const basketTemplate = Handlebars.compile(basketTemplateSource);
-  const basketHtml = basketTemplate(basketStore.getAllItems());
-  $('#views').html(basketHtml);
-
-  $('.remove-from-basket').on('click', handleRemoveFromBasket);
-}
-
-function addToBasket(productId) {
-  $.ajax({
-    url: '/basket/add',
-    type: 'POST',
-    data: { productId },
-    success: function (response) {
-      // Update the basket view or display a notification
-    },
-    error: function (xhr, status, error) {
-      console.error('Error adding product to basket:', error);
-    }
-  });
-}
-
-function handleRemoveFromBasket() {
-  const productId = $(this).data('product-id');
-  basketStore.removeItem(productId);
-  renderBasket();
-}
