af64807609098edf16ad146b3b63a76c0a18630d
Glitch (glitch-blank-website) <none>
2023-04-26 00:35:19 +0000

    🎤🍚 Checkpoint ./views/about.hbs:53479949/6988 ./controllers/about.js:53479949/1016

--- controllers/about.js
+++ controllers/about.js
@@ -38,35 +38,3 @@ export default about;
 
 
 
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
-
-<url>
-  <loc>https://furtive-common-bougon.glitch.me/</loc>
-  <lastmod>2023-04-26T00:09:09+00:00</lastmod>
-  <priority>1.00</priority>
-</url>
-<url>
-  <loc>https://furtive-common-bougon.glitch.me/dashboard</loc>
-  <lastmod>2023-04-26T00:09:09+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://furtive-common-bougon.glitch.me/about</loc>
-  <lastmod>2023-04-26T00:09:09+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>https://furtive-common-bougon.glitch.me/basket/a71d9dbb-ec22-42ec-a314-f002cb6aef91</loc>
-  <lastmod>2023-04-26T00:09:09+00:00</lastmod>
-  <priority>0.64</priority>
-</url>
-
-
-</urlset>
