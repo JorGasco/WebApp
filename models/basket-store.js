463ef18b1b3126d2f1400a8ffbe5a06bdfa3fb09
Glitch (glitch-blank-website) <none>
2023-05-01 06:35:00 +0000

    🏈🎹 Checkpoint ./models/basket-store.js:53479949/2042

--- models/basket-store.js
+++ models/basket-store.js
@@ -1,7 +1,19 @@
 'use strict';
-import cloudinary from 'cloudinary';
 import logger from '../utils/logger.js';
 import JsonStore from './json-store.js';
+import cloudinary from 'cloudinary';
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+try {
+  const env = require("../.data/.env.json");
+  cloudinary.config(env.cloudinary);
+}
+catch(e) {
+  logger.info('You must provide a Cloudinary credentials file - see README.md');
+  process.exit(1);
+}
 
 const basketStore = {
 
@@ -30,8 +42,21 @@ removeAllBaskets() {
 this.store.removeAll(this.collection);
 },
 
-addBasket(basket) {
-this.store.addCollection(this.collection, basket);
+ async addBasket(basket, response) {
+  function uploader(){
+    return new Promise(function(resolve, reject) {  
+      cloudinary.uploader.upload(basket.picture.tempFilePath,function(result,err){
+        if(err){console.log(err);}
+        resolve(result);
+      });
+    });
+  }
+  let result = await uploader();
+  logger.info('cloudinary result', result);
+  basket.picture = result.url;
+
+  this.store.addCollection(this.collection, basket);
+  response();
 },
 
 addProduct(id, product) {
